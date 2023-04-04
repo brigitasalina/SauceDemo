218ddcd3f86b81afdd217fa24bedd9630a9afd5f
ramintasalina <brigita.salina@gmail.com>
2023-04-04 09:30:35 +0300

    just in case

--- src/test/java/SauceDemoTest.java
+++ src/test/java/SauceDemoTest.java
@@ -1,23 +1,22 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
+
 import java.time.Duration;
+//import static org.testng.Assert.*;
+
 public class SauceDemoTest {
     public WebDriver driver;
 
     @Test
-
-    public void pageLoads() throws InterruptedException{
+    public void pageLoads(){
     driver.get("https://www.saucedemo.com/");
     driver.findElement(By.id("user-name")).sendKeys("standard_user");
     driver.findElement(By.id("password")).sendKeys("secret_sauce");
     driver.findElement(By.id("login-button")).click();
     driver.findElement(By.id("item_0_title_link")).click();
     driver.findElement(By.id("add-to-cart-sauce-labs-bike-light")).click();
-//    driver.findElement(By.id("remove-sauce-labs-bike-light")).click();
     driver.findElement(By.id("shopping_cart_container")).click();
     driver.findElement(By.id("checkout")).click();
     driver.findElement(By.id("first-name")).sendKeys("Brigita");
@@ -26,11 +25,12 @@ public class SauceDemoTest {
     driver.findElement(By.id("continue")).click();
     driver.findElement(By.id("finish")).click();
     driver.findElement(By.id("back-to-products")).click();
+   // assertEquals(driver.findElement(By.className("complete-header")).getText(),"Thank you for your order!");
     }
-    
+
     @BeforeClass
     public void beforeClass(){
-        System.setProperty("webdriver.com.driver","drivers\\chromedriver111.exe");
+        System.setProperty("webdriver.chrome.driver","drivers\\chromedriver111.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver. manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
